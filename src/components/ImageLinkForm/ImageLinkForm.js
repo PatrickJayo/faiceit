df18aa640181c2dac828f33ec134fc72d0183095
Patrick Jayoma <PatrickJayy@gmail.com>
2021-07-16 09:22:01 -0700

    Deploy

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'Copy the url of the image into the space below and Faiceit will detect the faces.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
